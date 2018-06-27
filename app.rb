98156a4322fcc5c49c1beec5b1d1d7975cb20711
iwilliams83 <indirajamurath@gmail.com>
2018-06-27 14:47:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/' do 
-    erb :new 
+  get '/' do
+    erb :new
+  end
+
+  post '/student' do 
+    erb :student
   end
 end
